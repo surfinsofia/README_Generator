6383e7b9bc4451da18ef04e8e4f6740567417d24
Sofia <asofiae96@gmail.com>
2022-06-17 10:52:35 -0500

    second commit

---
+++ Develop/README.md
@@ -0,0 +1,36 @@
+
+  ## Title
+  Password Generator
+  
+  ## Description
+  Generates Custom password
+
+  ## Table of Contents
+  1. [Installation](#Installation)
+  2. [Usage](#Usage)
+  3. [License](#License)
+  4. [Contributing](#Contributing)
+  5. [Tests](#Tests)
+  6. [Questions](#Questions)
+
+  ## Installation
+  click the button
+
+  ## Usage
+  create a safe pw
+
+  ## License
+  one
+
+  ## Contributing
+  make it more complex
+
+  ## Tests
+  do 1 2 3 jumpo
+
+  ## Questions
+  surfinsofia
+  aaa
+  asofia
+  
+  
--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,8 @@
 // TODO: Include packages needed for this application
-const inquirer = require ('inquirer');
-const fs = require ('fs');
+const inquirer = require('inquirer');
+const fs = require('fs');
 const util = require('util');
-const {generateMarkdown} = require ('./utils/generateMarkdown');
+const { generateMarkdown } = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
@@ -11,28 +11,85 @@ const init = () => {
 
     inquirer.prompt([
         {
-        name: "Title",
-        message: "What is the title of your project?",
-        type:"input",
-    },
+            name: "Title",
+            message: "What is the title of your project?",
+            type: "input",
+        },
+
         {
-            type: 'list', 
-            message: 'this the message', 
-            name: 'the name',
-            choices: ["one", "two"]
-        }
+            name: "Description",
+            message: "Briefly Describe your Project",
+            type: "input",
+        },
+
+        {
+            name: "Installation",
+            message: "What instructions should be followed for this project?",
+            type: "input",
+        },
+
+        {
+            name: "Usage",
+            message: "What is the usage of this project?",
+            type: "input",
+
+        },
+
+        {
+            type: 'list',
+            message: 'What is the License for this project?',
+            name: 'License',
+            choices: ["one", "two", "three"]
+        },
+
+        {
+            type: 'input',
+            message: 'How can users contribute to this project?',
+            name: "Contributing",
+
+        },
+
+        {
+            type:"input",
+            name: "Tests",
+            message: "Give test instructions",
+        },
+
+        {
+            type: 'input',
+            message: 'Enter your github username',
+            name: 'Questions',
+
+        },
+
+        {
+            type: 'input',
+            name: 'LinkedIn',
+            message: 'Enter your LinkedIn URL.',
+        },
+        {
+            type: 'input',
+            name: 'Email',
+            message: 'Enter your Email address',
+        },
+
+
+
+
+
     ])
-    .then(answers => {
-        writeToFile(answers)
-        console.log('Success!')
-    });}
+        .then(answers => {
+            writeToFile(answers)
+            console.log('Success! Your README.md file has been created!')
+        });
+}
 
 
 // TODO: Create a function to write README file
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-function writeToFile(answers)  {
+function writeToFile(answers) {
     writeFileAsync('README.md', generateMarkdown(answers))
 }
 
